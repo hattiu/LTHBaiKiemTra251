3a3818aa61d1016a41f48ff09af10180c20aeed5
hattiu <hattiu210817@gmail.com>
2023-08-04 10:43:13 +0700

    CreateLeThiHoa_Cau4

--- FirstWebMVC/Data/LeThiHoaDbContext.cs
+++
@@ -1,7 +0,0 @@
-namespace FirstWebMVC.Data
-{
-    public class LeThiHoaDbContext
-    {
-        
-    }
-}
--- FirstWebMVC/Models/PhongBan.cs
+++ FirstWebMVC/Models/PhongBan.cs
@@ -2,6 +2,7 @@ namespace FirstWebMVC.Models
 {
     public class PhongBan 
     {
+        
         public string MaPhongBan { get; set; }
         public string TenPhongBan { get; set; }
         
